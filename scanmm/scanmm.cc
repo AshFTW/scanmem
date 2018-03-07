0df9faf96a96d21fe6396048426c2921483a560a
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2018-03-08 00:02:29 +0100

    Empty

---
+++ scanmm/scanmm.cc
@@ -0,0 +1,5 @@
+//
+// Created by root on 23.03.18.
+//
+
+#include "scanmm.hh"
---
+++ scanmm/scanmm.hh
@@ -0,0 +1,16 @@
+//
+// Created by root on 23.03.18.
+//
+
+#ifndef REVERSE_ENGINE_SCANMM_HH
+#define REVERSE_ENGINE_SCANMM_HH
+
+
+
+class scanmm {
+
+};
+
+
+
+#endif //REVERSE_ENGINE_SCANMM_HH
